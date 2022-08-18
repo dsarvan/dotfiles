f72c3b884ada20fd4b19f52b446fb6659b56a301
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-08-18 21:21:27 +0530

    configuration file

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,23 @@
+config.load_autoconfig()
+c.url.default_page = "https://startpage.com/{}"
+c.url.start_pages = ["https://startpage.com/{}"]
+c.url.searchengines = {
+    "DEFAULT": "https://startpage.com/{}"
+}
+
+# https://www.reddit.com/r/qutebrowser/comments/esvo0l/use_my_font/
+c.fonts.tabs.selected = "10pt Iosevka Etoile"
+c.fonts.tabs.unselected = "10pt Iosevka Etoile"
+c.fonts.hints = "9pt Iosevka Etoile"
+c.colors.hints.bg = "#ffffff"
+c.fonts.keyhint = "10pt Iosevka Etoile"
+c.fonts.prompts = "10pt Iosevka Etoile"
+c.fonts.downloads = "10pt Iosevka Etoile"
+c.fonts.statusbar = "10pt Iosevka Etoile"
+c.fonts.contextmenu = "10pt Iosevka Etoile"
+c.fonts.messages.error = "9pt Iosevka Etoile"
+c.fonts.messages.info = "9pt Iosevka Etoile"
+c.fonts.messages.warning = "9pt Iosevka Etoile"
+c.fonts.debug_console = "10pt Iosevka Etoile"
+c.fonts.completion.entry = "10pt Iosevka Etoile"
+c.fonts.completion.category = "10pt Iosevka Etoile"
